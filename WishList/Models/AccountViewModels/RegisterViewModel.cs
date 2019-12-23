ca9aefefad1ed497b30da258c3ab622b6c5d1e49
david-rogers <dwrogers@uw.edu>
2019-12-22 16:21:05 -0800

    Do register functionality

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -13,11 +13,12 @@ namespace WishList.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, MinimumLength=6)]
+        [StringLength(100, MinimumLength=8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
 
+        [Required]
         [DataType(DataType.Password)]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
